e16d2c0e096c44a214485706cb90d819cc0029a4
Savero Pakavi <revaztemellies@mbp15-spz.local>
2025-04-09 07:31:52 +0700

    Add logo folder for staticfiles

--- hashmicro_test/settings.py
+++ hashmicro_test/settings.py
@@ -14,6 +14,8 @@ ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://hm-technical-test-production.up.railway.app',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 ]
 
 INSTALLED_APPS = [
